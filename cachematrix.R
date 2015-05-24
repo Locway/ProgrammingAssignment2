f6fb54936312fcb696392c966c3ee80228d4a0cb
Locway <Locway@hotmail.com>
2015-05-24 07:20:28 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# the functions below will read and invert a matrix, placing any solved matrix into cache so that it need not be 
+# resolved everytime it is needed
 
 ## Write a short comment describing this function
+# The makeCacheMatrix function below sets up and intialises a matrix in cache, it also creates set, get, 
+# setInverted and getInverted functions for use
 
 makeCacheMatrix <- function(x = matrix()) {
 xinverted <- NULL #prebuilding where the result of the inverted matrix is stored
@@ -17,6 +21,8 @@ xinverted <- NULL #prebuilding where the result of the inverted matrix is stored
 
 
 ## Write a short comment describing this function
+# cacheSolve will test if the matrix inversion has already been solved, if yes then it returns this from cache
+# otherwise it will solve for the inverse
 
 cacheSolve <- function(x, ...) {
         mtx <- x$getInvert() #get the the inverted matrix
